529011c78af914c82bfd5962e3471e4dd3c79f72
Alexander Skvortsov <askvortsov1@users.noreply.github.com>
2021-02-28 03:35:25 +0000

    Apply fixes from StyleCI

--- extend.php
+++ extend.php
@@ -1,14 +1,12 @@
 <?php
 
 /*
- * This file is part of askvortsov/flarum-markdown-tables.
+ * This file is part of askvortsov/flarum-markdown-tables
  *
- * Copyright (c) 2019 
- * Extension by DogSports
- * Updated by Askvortsov.
+ *  Copyright (c) 2021 Alexander Skvortsov.
  *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
+ *  For detailed copyright and license information, please view the
+ *  LICENSE file that was distributed with this source code.
  */
 
 namespace Askvortsov\MarkdownTables;
@@ -18,13 +16,13 @@ use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__ . '/js/dist/forum.js')
-        ->css(__DIR__ . '/resources/less/forum.less'),
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
 
-    new Extend\Locales(__DIR__ . '/resources/locale'),
+    new Extend\Locales(__DIR__.'/resources/locale'),
 
-    (new Extend\Formatter)
+    (new Extend\Formatter())
         ->configure(function (Configurator $config) {
             $config->PipeTables;
-        })
+        }),
 ];
